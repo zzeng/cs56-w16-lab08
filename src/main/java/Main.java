b15a84ee0dfdb58c85485e79738a674fd5bfd629
Ziliang Zeng <zzeng@umail.ucsb.edu>
2016-02-18 17:57:33 -0800

    added /db route

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -27,10 +27,39 @@ public class Main {
 
     get("/hello", (req, res) -> {
 	    RelativisticModel.select();
+
+	    String energy = System.getenv().get("ENERGY");
+	    
 	    Amount<Mass> m = Amount.valueOf("12 GeV").to(KILOGRAM);
 	    return "E=mc^2: 12 GeV = " + m.toString();
 	});
     get("/ucsb", (req, res) -> "Go Gauchos");
+
+    get("/db", (req, res) -> {
+      Connection connection = null;
+      Map<String, Object> attributes = new HashMap<>();
+      try {
+        connection = DatabaseUrl.extract().getConnection();
+
+        Statement stmt = connection.createStatement();
+        stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+        stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+        ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+
+        ArrayList<String> output = new ArrayList<String>();
+        while (rs.next()) {
+          output.add( "Read from DB: " + rs.getTimestamp("tick"));
+        }
+
+        attributes.put("results", output);
+        return new ModelAndView(attributes, "db.ftl");
+      } catch (Exception e) {
+        attributes.put("message", "There was an error: " + e);
+        return new ModelAndView(attributes, "error.ftl");
+      } finally {
+        if (connection != null) try{connection.close();} catch(SQLException e){}
+      }
+    }, new FreeMarkerEngine());
     
     get("/", (request, response) -> {
             Map<String, Object> attributes = new HashMap<>();
