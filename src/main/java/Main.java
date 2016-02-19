c67b1bddbab073cf7b88b735769be2e93d0f5eea
Ziliang Zeng <zzeng@umail.ucsb.edu>
2016-02-18 17:42:02 -0800

    Demo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,6 +13,11 @@ import static spark.Spark.get;
 
 import com.heroku.sdk.jdbc.DatabaseUrl;
 
+import static javax.measure.unit.SI.KILOGRAM;
+import javax.measure.quantity.Mass;
+import org.jscience.physics.model.RelativisticModel;
+import org.jscience.physics.amount.Amount;
+
 public class Main {
 
   public static void main(String[] args) {
@@ -20,7 +25,11 @@ public class Main {
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
 
-    get("/hello", (req, res) -> "Hello World");
+    get("/hello", (req, res) -> {
+	    RelativisticModel.select();
+	    Amount<Mass> m = Amount.valueOf("12 GeV").to(KILOGRAM);
+	    return "E=mc^2: 12 GeV = " + m.toString();
+	});
     get("/ucsb", (req, res) -> "Go Gauchos");
     
     get("/", (request, response) -> {
